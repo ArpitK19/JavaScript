8305072bb71752d740dc2379e76159efb46b1c81
Arpit Kansara <109519873+ArpitK19@users.noreply.github.com>
2022-11-11 15:59:10 +0530

    DOM - GET SET & query selector

---
+++ main.js
@@ -0,0 +1,67 @@
+// using id
+
+// var a;
+
+// a=document.getElementById("header");
+// console.log(a);
+
+// // using class
+
+// var b;
+
+// b=document.getElementsByClassName("nav");
+// console.log(b);
+
+// // using tag name
+
+// var c;
+
+// c=document.getElementsByTagName("a");
+// console.log(c); 
+
+
+// using GET Method
+
+var header;
+
+// header=document.getElementById("header").innerText;
+// console.log(header);
+
+// header=document.getElementById("header").innerHTML;
+// console.log(header);
+
+// header=document.getElementById("n").getAttribute("style");
+// console.log(header);
+
+// header=document.getElementById("n").getAttributeNode("class");
+// console.log(header);
+
+// header=document.getElementById("n").attributes;
+// console.log(header);
+
+// SET Method
+
+// header=document.getElementById("header").innerHTML="Shivam Kansara";
+// console.log(header);
+
+// class name change
+// header=document.getElementById("n").setAttribute("class","navi");
+// console.log(header);
+// head=document.getElementById("n").getAttribute("class");
+// console.log(head);
+
+// header=document.getElementById("n").getAttribute("class","");
+
+
+
+// New DOM Method
+
+// var a;
+// a=document.querySelector(".nav").innerHTML;
+// console.log(a);
+
+var a;
+a=document.querySelectorAll(".mainlist");
+console.log(a[3]);
+
+
